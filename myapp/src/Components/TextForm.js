52aa00bea6269165f58c57add3f949b71ebf9c81
jahnvi_sah <100960142+jahnvisah@users.noreply.github.com>
2022-06-27 13:33:20 +0530

    Update TextForm.js

--- myapp/src/Components/TextForm.js
+++ myapp/src/Components/TextForm.js
@@ -8,6 +8,11 @@ export default function(props){
     const handleOnChange = (event) =>{
         setText(event.target.value);
     }
+
+    const handleClearClick =(event) =>{
+        let newText="";
+        setText(newText);
+    }
    
     const[text,setText] = useState("Enter text here");
   return (
@@ -16,7 +21,8 @@ export default function(props){
         <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox"></textarea>
   </div>
-  <button className="btn-btn-primary" onClick={handleUpClick}>Convert</button>
+  <button className="btn-btn-primary mx-2" onClick={handleUpClick}>Convert</button>
+  <button className="btn-btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
 </div>
     
   )
